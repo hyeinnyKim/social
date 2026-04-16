811679506b7241d9901f203e54ee7b37aeeb1fdf
hyeinnyKim <hyein0108@gmail.com>
2026-04-16 16:56:14 +0900

    0416-second

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
-$(function(){
-    $("header nav ul li.icon i").on("click", function () {
-   $("header nav").hide();  
-    });
-    $("header .end .box a.menu").on("click", function () {
-      $("header nav").show();
-    });
-})
+$(function () {
+  $("header nav ul li.icon i").on("click", function () {
+    $("header nav").hide();
+  });
+  $("header .end .box a.menu").on("click", function () {
+    $("header nav").show();
+  });
+});
